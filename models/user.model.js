7c2b966345a5f408b81f2a11dd6945ee16207e23
PrathameshRSH26 <prathamesh26K10@gmail.com>
2026-04-06 21:47:16 +0530

    Designing and Implementing the User Schema using Drizzle ORM

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,14 @@
-import { pgTable, text, serial } from "drizzle-orm/pg-core";
+import { pgTable, text, serial, varchar, timestamp } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
-  name: text("name"),
+  firstname: varchar("firstname",{length: 55}).notNull(),
+  lastname: varchar('last_name',{length: 55}),
+  email: varchar({length: 55}).notNull().unique(),
+  password: text().notNull(),
+  salt: text().notNull(),
+
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').$onUpdate(()=> new Date()),
+
 });
